7f031358fa079e8f55eedac3bccffc0379606b71
Tom Lindley <tom@tlkr.co.uk>
2021-07-18 22:45:22 +0100

    added autorun script

---
+++ autorun.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+UPDATE_CHECK_DELAY=${1:-60}
+PID=""
+CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd "$CURRENT_DIR"
+echo "Checking for updates every $UPDATE_CHECK_DELAY seconds"
+echo "Starting..."
+
+while true; do
+  git fetch
+  GIT_REMOTE_BRANCH="origin/$(git rev-parse --abbrev-ref HEAD)"
+  CURRENT=$(git log -1 --pretty=format:"%h")
+  ORIGIN=$(git log -1 --pretty=format:"%h" "$GIT_REMOTE_BRANCH")
+
+  if [ "$CURRENT" == "$ORIGIN" ] && [ "$PID" != "" ]; then
+      sleep "$UPDATE_CHECK_DELAY"
+  else
+    if [ "$PID" != "" ]; then
+      kill "$PID"
+    fi
+    git reset --hard
+    git clean -df
+    git pull
+    npm ci
+    node index.js &
+    PID=$?
+  fi
+
+done
